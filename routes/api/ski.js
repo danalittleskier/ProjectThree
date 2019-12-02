c4e7b7bcc8836f1f9eb4f79acafbc943d8ceb509
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-12-02 12:15:46 -0700

    Route for returning the ski by snowtype

--- routes/api/ski.js
+++ routes/api/ski.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const skiDBController = require("../../controllers/skiDBController")
 
 
-router.get('/ski', (req, res) => res.send('ski route enabled'))
+//router.get('/ski', (req, res) => res.send('ski route enabled'))
 
-
+router.route("/", (req, res) => {
+  console.log(req.body);
+  res.send(skiDBController.findByCategory(req.body.category));
+})
+  // .get(skiDBController.findByCategory(req.body.category));
 
 
 module.exports = router;
