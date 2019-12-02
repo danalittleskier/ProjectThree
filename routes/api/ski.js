0d890923a696d3f749f578990daf3f494f4528bb
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-12-02 15:20:25 -0700

    Tracking errors

--- routes/api/ski.js
+++ routes/api/ski.js
@@ -6,8 +6,8 @@ const skiDBController = require("../../controllers/skiDBController")
 //router.get('/ski', (req, res) => res.send('ski route enabled'))
 
 router.route("/", (req, res) => {
-  console.log(req.body);
-  res.send(skiDBController.findByCategory(req.body.category));
+  //console.log(req.body.snowtype);
+  res.send(skiDBController.findByCategory(req.body.snowtype));
 })
   // .get(skiDBController.findByCategory(req.body.category));
 
