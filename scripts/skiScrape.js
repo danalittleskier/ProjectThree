24fc2d9b49b264295a1d6dc8f654932a0b780189
tr3vbr0w <trevbrow@live.com>
2019-12-05 15:58:45 -0700

    modified carousel, removing images below and l/r buttons

--- scripts/skiScrape.js
+++ scripts/skiScrape.js
@@ -1,44 +1,44 @@
-var cheerio = require("cheerio");
+// var cheerio = require("cheerio");
 
-app.get("/ski-scrape", function (req, res) {
-    //Use axios to grab the body of html
-    var skis = [];
-    axios.get("https://www.backcountry.com/alpine-skis").then(function (response) {
-        //Save cheerio to $ to run like jQuery
-        var $ = cheerio.load(response.data);
-        //Grab every CHILD HTML TAG within a PARENT HTML TAG, follows cheerio syntax from documentation here
-        $(".ui-product-listing-grid").each(function (i, element) {
+// app.get("/ski-scrape", function (req, res) {
+//     //Use axios to grab the body of html
+//     var skis = [];
+//     axios.get("https://www.backcountry.com/alpine-skis").then(function (response) {
+//         //Save cheerio to $ to run like jQuery
+//         var $ = cheerio.load(response.data);
+//         //Grab every CHILD HTML TAG within a PARENT HTML TAG, follows cheerio syntax from documentation here
+//         $(".ui-product-listing-grid").each(function (i, element) {
 
-            //Save the results to a local object
-            var result = {}
-            //Add text of every link, save as properties of a result object
-            result.brand = $(this)
-                .find("div")
-                .hasClass("product");
+//             //Save the results to a local object
+//             var result = {}
+//             //Add text of every link, save as properties of a result object
+//             result.brand = $(this)
+//                 .find("div")
+//                 .hasClass("product");
                 
                 
-            console.log(result.brand);
-            result.model = $(this)
-                .children()
-                .text();
-            result.image = $(this)
-                .find("img")
-                .attr("src")
-            // result.category = $(this)
-            //     .children()
-            //     .text();
-            // result.brand = $(this)
-            //     .children()
-            //     .text();
-            // result. = "https://www.backcountry.com/alpine-skis" + $(this)
-            //     .find("a")
-            //     .attr("href");
+//             console.log(result.brand);
+//             result.model = $(this)
+//                 .children()
+//                 .text();
+//             result.image = $(this)
+//                 .find("img")
+//                 .attr("src")
+//             // result.category = $(this)
+//             //     .children()
+//             //     .text();
+//             // result.brand = $(this)
+//             //     .children()
+//             //     .text();
+//             // result. = "https://www.backcountry.com/alpine-skis" + $(this)
+//             //     .find("a")
+//             //     .attr("href");
             
-            // db.Resort.create(result)
-            //     .catch(function (err) {
-            //         res.sendStatus(err);
-            //     });
-        });
-        res.send("Scrape Complete")
-    });
-});
+//             // db.Resort.create(result)
+//             //     .catch(function (err) {
+//             //         res.sendStatus(err);
+//             //     });
+//         });
+//         res.send("Scrape Complete")
+//     });
+// });
