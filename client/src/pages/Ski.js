127615ec8d8df9d13c39756839235b7aeec0b80b
gerritt <gmocore@gmail.com>
2019-12-14 12:21:59 -0700

    skitails styling

--- client/src/pages/Ski.js
+++ client/src/pages/Ski.js
@@ -24,26 +24,40 @@ class Ski extends Component {
       <Container>
         <Row>
           {console.log(this.state.ski)}
-          <div className="col s12 m12">
-            <div className="card medium">
+          <div className="col s12 m12 card">
+            <div className="">
               <div className="card-image">
-                <img src={this.state.ski.image} alt="ski model" />
+                <img
+                  src={this.state.ski.image}
+                  className="responsive-img"
+                  alt="ski model"
+                />
                 <span className="card-title">
                   {this.state.ski.brand} <span>{this.state.ski.model} </span>
                 </span>
               </div>
-              <div className="card-content">
-                <h6>{this.state.ski.category}</h6>
-                <p>Snow: {this.state.ski.snow_recommendation}</p>
-                <p>pace: {this.state.ski.pace_recommendation}</p>
-                <p>Turns: {this.state.ski.turns_recommendation}</p>
-                <p>Gender: {this.state.ski.gender}</p>
-                <p>Description: {this.state.ski.description}</p>
-                <p>width: {this.state.ski.width}</p>
-                <p>Length: {this.state.ski.length}</p>
-                <p>radius: {this.state.ski.radius}</p>
-                <p>tip: {this.state.ski.tip}</p>
-                <p>tail: {this.state.ski.tail}</p>
+              <div className="row">
+                <h5 className="center">{this.state.ski.category}</h5>
+              </div>
+              <div className="row center">
+                <div className="col s6">
+                  <h5>Details</h5>
+                  <p>Snow: {this.state.ski.snow_recommendation}</p>
+                  <p>Pace: {this.state.ski.pace_recommendation}</p>
+                  <p>Turns: {this.state.ski.turns_recommendation}</p>
+                  <p>Gender: {this.state.ski.gender}</p>
+                </div>
+                <div className="col s6">
+                  <h5>Dimensions</h5>
+                  <p>Width: {this.state.ski.width}</p>
+                  <p>Length: {this.state.ski.length}</p>
+                  <p>Radius: {this.state.ski.radius}</p>
+                  <p>Tip: {this.state.ski.tip}</p>
+                  <p>Tail: {this.state.ski.tail}</p>
+                </div>
+              </div>
+              <div className="container">
+                <p>{this.state.ski.description}</p>
               </div>
             </div>
           </div>
